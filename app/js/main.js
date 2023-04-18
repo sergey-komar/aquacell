487b85715acf33331115c269230c3afeb3f9f53e
sergey-komar <kursev81@yandex.ru>
2023-04-18 22:43:15 +0300

    home page

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,10 @@
 $(function () {
-
+$('.blog-home__slider-inner').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    appendArrows: '.blog-home__arrows',
+    prevArrow: '<button type="button" class="slick-prev"></button>',
+    nextArrow: '<button type="button" class="slick-next"></button>',
+    dots: true
+})
 })
